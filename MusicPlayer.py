31b0756d3dc1a2838c30e12d6772c777ffb4541b
DANISH QAMAR <50513044+danish1512@users.noreply.github.com>
2020-12-27 12:59:39 +0530

    Add files via upload

---
+++ MusicPlayer.py
@@ -0,0 +1,11 @@
+git log
+git commit MusicPlayer.py - m FirstPython
+git config --global user.email "mddanishqamar179@gmail.com"
+git config --global user.name "danish1512"
+git --config --global --list
+git remote origin https://github.com/danish1512/MusicPlayerProject.git
+git remote add origin
+git push
+git remote show origin
+git push --upstream origin master
+git status
